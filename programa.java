5f607811e91758cf581c55aba753da9c6aa74a92
luanbela <101910479+luanbela@users.noreply.github.com>
2022-04-03 21:15:12 -0300

    Circunferência da terra com java.

---
+++ programa.java
@@ -0,0 +1,15 @@
+public class programa{
+    public static void main(String []args) {
+        System.out.println("calculando circunferência da terra...");
+        float pi = 3.14f;
+        float raterra = 6.371f;
+        float circun = 2 * pi * raterra;
+        
+
+
+
+        System.out.println(circun);
+
+
+    }
+}
